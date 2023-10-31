24d078338f8e7d86682df27f949edab7a4205650
AnastasiiaMarchuk <stasijushka5@gmail.com>
2023-10-31 20:11:45 +0200

    finished

---
+++ README.md
@@ -0,0 +1,10 @@
+1. node index.js --action="list"
+https://monosnap.com/file/gSwW4FM5dhO36uxGGLLt5oqUgtvdYL
+
+2. + 3. 
+node index.js --action="get" --id 05olLMgyVQdWRwgKfg5J6
+node index.js --action="add" --name Mango --email mango@gmail.com --phone 322-22-22
+https://monosnap.com/file/OWVehgDXqIGSJByGLG9kA7yHaVLn2F
+
+4. node index.js --action="remove" --id qdggE76Jtbfd9eWJHrssH
+https://monosnap.com/file/apVgDkHRJceCQLlzTqJwll9oRTSjut
---
+++ contacts.js
@@ -0,0 +1,59 @@
+const fs = require("node:fs/promises");
+const path = require("node:path");
+const crypto = require("node:crypto");
+
+const contactsPath = path.join(__dirname, 'db', 'contacts.json');
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, { encoding: "UTF-8" });
+    const contacts = JSON.parse(data);
+    return contacts;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    return contacts.find((contact) => contact.id === contactId) || null;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex((contact) => contact.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+    const [removedContact] = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return removedContact;
+  } catch (error) {
+    throw error;
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+    const contacts = await listContacts();
+    const newContact = { id: crypto.randomUUID(), name, email, phone };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return newContact;
+  } catch (error) {
+    throw error;
+  }
+}
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
+};
+
